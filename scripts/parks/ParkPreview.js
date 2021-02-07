86c7627010530c712d9a84b6c0d57b6f6a2635b4
Ccourter11 <courtercalvin@yahoo.com>
2021-02-06 21:24:54 -0600

    got some color up, got the drop down rendering, created more files for structure and am going to relax now

---
+++ scripts/parks/ParkPreview.js
@@ -0,0 +1,22 @@
+import { Park } from "./Park.js";
+import { useParks, getParks } from "./ParkProvider.js";
+import "./ParkSelect.js";
+
+const eventHub = document.querySelector(".container");
+const parkContainer = document.querySelector(".parkPreview");
+
+eventHub.addEventListener("parkSelected", (changeEvent) => {
+  const selectedParkName = changeEvent.detail.selectedPark;
+  const parkArray = useParks();
+  const theSelectedPark = parkArray.find(
+    (parksObj) => parksObj.id === selectedParkName
+  );
+  render(theSelectedPark);
+});
+
+const render = (parksObj) => {
+  const parkHTMLrepresentation = Park(parksObj);
+  parkContainer.innerHTML = `
+    ${parkHTMLrepresentation}
+    `;
+};
